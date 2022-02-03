255761ca5296c89102b8c85b13b273bdbb10d699
dane <bc511tm@yahoo.it>
2022-02-03 16:31:03 +0100

    Filter for reversing a String implemented.

--- app/controllers/NinjaController.controller.js
+++ app/controllers/NinjaController.controller.js
@@ -1,28 +1,31 @@
-
 /* Controller principale dell'applicazione 
    Lo scope serve per il two way binding quindi HTML view = variabile JS 
 */
-angular.module('NinjaController', ['ninjaService'])
-.controller('NinjaController', ['$scope', 'ninjaService', function($scope, ninjaService){
-    $scope.message = "Type to filter search: ";
-    
-  
-    ninjaService.getNinjas().then(function(data){
+angular
+  .module("NinjaController", ["ninjaService", "reverseFilter"])
+  .controller("NinjaController", [
+    "$scope",
+    "ninjaService",
+    "reverseFilter",
+    function ($scope, ninjaService, reverseFilter) {
+      $scope.message = "Type to filter search: ";
+      $scope.reversedMessage = reverseFilter("Check out our random ninja!");
+
+      ninjaService.getNinjas().then(function (data) {
         $scope.ninjas = data;
-    });
-    
+      });
 
-    $scope.removeNinja = function(ninja){
+      $scope.removeNinja = function (ninja) {
         ninjaService.removeNinja($scope.ninjas, ninja);
-    };
+      };
 
-    $scope.addNinja = function(){
+      $scope.addNinja = function () {
         ninjaService.addNinja($scope.ninjas, $scope.newNinja);
         $scope.newNinja = ninjaService.clearNewNinja();
-    };
+      };
 
-    $scope.removeAll = function(){
+      $scope.removeAll = function () {
         $scope.ninjas = ninjaService.removeAll();
-    };
-
-}]);
+      };
+    },
+  ]);
---
+++ app/filters/reverseFilter.filter.js
@@ -0,0 +1,15 @@
+//Custom Pipes (FILTER in AngularJS)
+angular.module("reverseFilter", []).filter("reverse", function () {
+  return function (input, uppercase) {
+    input = input || "";
+    var out = "";
+    for (var i = 0; i < input.length; i++) {
+      out = input.charAt(i) + out;
+    }
+
+    if (uppercase) {
+      out = out.toUpperCase();
+    }
+    return out;
+  };
+});
