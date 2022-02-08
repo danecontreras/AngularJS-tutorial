e346e09d124cac021609b60eb8e7e242ca7f8e90
dane <bc511tm@yahoo.it>
2022-02-08 10:28:43 +0100

    Single components for list
    Bug: single remove button not working

--- app/components/ninja/ninja.component.js
+++ app/components/ninja/ninja.component.js
@@ -1,4 +1,7 @@
 angular.module("ninjaComponent", []).component("ninjaComponent", {
+  bindings: {
+    ninja: "=",
+  },
   templateUrl: "app/components/ninja/ninja.template.html",
   controller: "NinjaController",
 });
